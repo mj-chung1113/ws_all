d95147badab7c0486980c61de95b64d36ad61fe2
mj-chung1113 <jmj8521@gmail.com>
2025-06-16 13:31:36 +0900

    lost files foundgit checkout

---
+++ ws_px4_controls/build/px4_msgs/px4_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/acdl1/ws_all/ws_px4_controls/build/px4_msgs/px4_msgs__py
+# Build directory: /home/acdl1/ws_all/ws_px4_controls/build/px4_msgs/px4_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
